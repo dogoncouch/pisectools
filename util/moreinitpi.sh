be7946a89b3d1b2d9319e6717f3998f02c540e61
dogoncouch <dpersons617@gmail.com>
2017-04-04 22:03:47 -0400

    Update moreinitpi.sh

--- util/moreinitpi.sh
+++ util/moreinitpi.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-sudo apt install vim-doc macchanger imagemagick mpg123 wifite tcpdump tcpreplay python-pcapy wireshark wireshark-doc
+sudo apt-get install -y vim-doc macchanger imagemagick mpg123 wifite tcpdump tcpreplay python-pcapy wireshark wireshark-doc
 
 # sudo apt-get install -y lm-sensors psensor
 
