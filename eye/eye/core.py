41263e67a38fbdc7a4106f92b4c1791209cffdd4
dogoncouch <dpersonsdev@gmail.com>
2017-08-26 13:29:53 -0400

    Fix syntax error

--- eye/eye/core.py
+++ eye/eye/core.py
@@ -252,7 +252,7 @@ class LisardEyeCore:
                         vlength = vllist[1] + ':' + vllist[2].split('.')[0]
                         syslog.syslog(syslog.LOG_INFO,
                                 'Video: Split: ' + self.file_name + \
-                                        'length: ' vlength)
+                                        'length: ' + vlength)
                         self.cam.stop_cam()
                         syslog.syslog(syslog.LOG_INFO,
                                 'Video: Stopped: ' + self.file_name + \
